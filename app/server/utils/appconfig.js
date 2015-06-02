407971be33dc7baf4c2f31411893e9c7fb9f9d6b
Manuel Deval <manuel.deval@gmail.com>
2015-06-02 20:45:59 +0200

    J'ai oublie de commiter le wrapper...

---
+++ app/server/utils/appconfig.js
@@ -0,0 +1,22 @@
+var config = require('config');
+
+var _replaceDotBy_dot_String = function(key){
+	return key.replace(/\./g,'_dot_');
+}
+
+// En variable d'environnement les '.' doivent être remplacés 
+// par la chaine '_dot_'
+var get = function(key){
+	var envKey = _replaceDotBy_dot_String(key);
+	if (process.env[envKey]){
+		return process.env[envKey];
+	} else {
+		return config.get(key);
+	}
+}
+
+module.exports = {
+	get:get
+}
+
+
