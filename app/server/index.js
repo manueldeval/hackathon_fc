9bd0e6d0c23903c7e7f9d613928779be4ff08b6f
manueldeval <manuel.deval@gmail.com>
2015-06-02 18:08:20 +0200

    externalisation de config

--- app/server/index.js
+++ app/server/index.js
@@ -1,4 +1,13 @@
+
+var config = require('config');
 var server = require('./server');
 var checkRouter = require('./api/checkrouter');
 
 server.use('/api', checkRouter);
+
+// Test de config
+console.log("k1: ",config.get('key1'));
+console.log("k2: ",config.get('key2'));
+console.log("k3: ",config.get('key3'));
+
+
