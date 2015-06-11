b74b798263e4a0cc6fe904d26e7a0ce1380c4464
Laurent Billiotte <laurent.billiotte@gmail.com>
2015-06-11 15:05:16 +0200

    test design

--- app/client/config/config.js
+++ app/client/config/config.js
@@ -8,14 +8,14 @@ var configTheme = function($mdThemingProvider) {
 }
 var configRoutes = function($routeProvider) {
 	// Configure routes
-	$routeProvider.when('/view1',{
-		templateUrl: 'views/view1.html',
+	$routeProvider.when('/accueil',{
+		templateUrl: 'views/accueil.html',
 		controller: 'ctrl1'
 	}).when('/view2',{
 		templateUrl: 'views/view2.html',
 		controller: 'ctrl2'
 	}).otherwise({
-		redirectTo: '/view1'
+		redirectTo: '/accueil'
 	});
 }
 
