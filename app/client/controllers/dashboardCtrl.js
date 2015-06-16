bc449be87073b486edf224e9ec71a885240eb177
Laurent Billiotte <laurent.billiotte@gmail.com>
2015-06-16 17:46:33 +0200

    show/hide

--- app/client/controllers/dashboardCtrl.js
+++ app/client/controllers/dashboardCtrl.js
@@ -11,10 +11,10 @@ var dashboardCtrl = function($scope, $location, dataService){
 	  	identite.birthdate=getLabVal('Date de naissance', $scope.app.user.birthdate);
 		identite.address=getLabVal('Adresse', $scope.app.user.address);
 		identite.phone=getLabVal('Téléphone', $scope.app.user.phone_number);
-		$scope.dashboards.push({id:'identite', label:'Identité', dash:identite, row:2, icone:'profile.svg'});
+		$scope.dashboards.push({id:'identite', label:'Identité', dash:identite, show:true, row:2, icone:'profile.svg'});
 	}
 	var getUserBanque = function() {
-		var dashboard = {id:'banque', label:'Coordonnées Bancaires', dash:{}, loading:true, row:1, icone:'credit.svg'};
+		var dashboard = {id:'banque', label:'Coordonnées Bancaires', dash:{}, loading:true, show:true, row:1, icone:'credit.svg'};
 		$scope.dashboards.push(dashboard);
 		dataService.getFromDataset('Banque_Coordonnees')
 		           .then(function(datas) {
@@ -23,7 +23,7 @@ var dashboardCtrl = function($scope, $location, dataService){
 		           })
 	}
 	var getUserFai = function() {
-		var dashboard = {id:'fai', label:'FAI', dash:{}, loading:true, row:1, icone:'fai.svg'}
+		var dashboard = {id:'fai', label:'FAI', dash:{}, loading:true, show:true, row:1, icone:'fai.svg'}
 		$scope.dashboards.push(dashboard);
 		dataService.getFromDataset('FAI_Contact')
 		           .then(function(datas) {
@@ -32,7 +32,7 @@ var dashboardCtrl = function($scope, $location, dataService){
 		           })
 	}
 	var getUserCasier = function() {
-		var dashboard = {id:'casier', label:'Casier Judiciaire', dash:{}, loading:true, row:1, icone:'casier.svg'}
+		var dashboard = {id:'casier', label:'Casier Judiciaire', dash:{}, loading:true, show:true, row:1, icone:'casier.svg'}
 		$scope.dashboards.push(dashboard);
 		dataService.getFromDataset('MJ_Casier')
 		           .then(function(datas) {
@@ -41,7 +41,7 @@ var dashboardCtrl = function($scope, $location, dataService){
 		           })
 	}
 	var getUserAcossSituationPro = function() {
-		var dashboard = {id:'situPro', label:'Situation professionnelle', dash:{}, loading:true, row:2, icone:'travail.svg'}
+		var dashboard = {id:'situPro', label:'Situation professionnelle', dash:{}, loading:true, show:true, row:2, icone:'travail.svg'}
 		$scope.dashboards.push(dashboard);
 		dataService.getFromDataset('ACOSS_SituationPro')
 		           .then(function(datas) {
@@ -60,6 +60,10 @@ var dashboardCtrl = function($scope, $location, dataService){
 	getUserFai();
 	getUserCasier();
 	getUserAcossSituationPro();
+
+	$scope.toggleDashboard = function(dashboard) {
+		dashboard.show=!dashboard.show;
+	}
 }
 
 
